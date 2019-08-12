7cc7e68c96473e92a7c63bd5fa9009eba1ab6578
Jacob Allen <boat.mon@gmail.com>
2019-08-12 16:35:34 -0400

    working on getting a image disply and file saving style work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    selectedFile: null
+  }
+  fileSelectedHandler = event => {
+    this.setState({})
+    selectedFile: event.target.files[0]
+  }
+
+  fileuploadHandler = () => {
+    axios.post('')
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input type="file" onChange={this.fileSelectedHandler} />
+        <button onClick={this.fileuploadHandler}>Upload</button>
+      </div>
+    )
+  }
 }
 
 export default App;
